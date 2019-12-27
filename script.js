6526e443b65fc2f01979a581a2d52781049b7b75
LeTigre <muzloserg@gmail.com>
2019-12-27 02:47:49 +0200

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict'
+// let answerByUser = +prompt("Are you gay?", "no, you!");
+
+// let arr = ['kek.jpg', 'orange.png', 'apple.bmp'];
+// console.log("arr" + " = object");
+// console.log(4 + +" = object");
+// console.log(answerByUser);
+// console.log(typeof (answerByUser));
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
